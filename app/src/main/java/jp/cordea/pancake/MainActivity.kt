49014e2c96192dc0f4698153e487bb118f6f311d
Yoshihiro Tanaka <contact@cordea.jp>
2018-05-10 21:31:14 +0900

    initial commit :)

---
+++ app/src/main/java/jp/cordea/pancake/MainActivity.kt
@@ -0,0 +1,19 @@
+package jp.cordea.pancake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.cordea.pancake.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/jp/cordea/pancake/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package jp.cordea.pancake.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
